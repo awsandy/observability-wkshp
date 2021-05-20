e6c7546f4be6cea7f590fca6f1e660c70d9962bd
awsandy <awsandy@amazon.co.uk>
2021-05-20 18:00:13 +0000

    all

--- 154-helm.sh
+++ 154-helm.sh
@@ -6,7 +6,7 @@ AWS_REGION=$(aws configure get region)
 echo $WORKSPACE_ID $IAM_PROXY_PROMETHEUS_ROLE_ARN $AWS_REGION
 # latest version 14.0.0 throws an erro - so try 13.8.0
 
-helm install --version 13.6.0 amp-prometheus-chart prometheus-community/prometheus -n prometheus -f ./getme-from-AMP-console.yaml \
+helm install amp-prometheus-chart prometheus-community/prometheus -n prometheus -f ./getme-from-AMP-console.yaml \
 --set serviceAccounts.server.annotations."eks\.amazonaws\.com/role-arn"="${IAM_PROXY_PROMETHEUS_ROLE_ARN}" \
 --set server.sidecarContainers.aws-sigv4-proxy-sidecar.args="{--name,aps,--region,${AWS_REGION},--host,aps-workspaces.${AWS_REGION}.amazonaws.com,--port,:8005}" \
 --set server.remoteWrite[0].url="http://localhost:8005/workspaces/${WORKSPACE_ID}/api/v1/remote_write" 
--- 155-adot.sh
+++ 155-adot.sh
@@ -1,4 +1,20 @@
-kubectl apply -f amp-eks-adot-prometheus-daemonset.yaml
+WORKSPACE_ID=$(aws amp list-workspaces --alias observability-workshop | jq .workspaces[0].workspaceId -r)
+AMP_ENDPOINT_URL=$(aws amp describe-workspace --workspace-id $WORKSPACE_ID | jq .workspace.prometheusEndpoint -r)
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+echo $WORKSPACE_ID
+echo $AMP_ENDPOINT_URL
+echo $AWS_ACCOUNT_ID
+echo $AWS_REGION
+AMP_REMOTE_WRITE=${AMP_ENDPOINT_URL}api/v1/remote_write
+echo $AMP_REMOTE_WRITE
+cp -f resources/amp-eks-adot-prometheus-daemonset.yaml daemonset.yaml
+sed -i -e "s/<AWS_ACCOUNT_ID>/$ACCOUNT_ID/g" daemonset.yaml
+sed -i -e "s/<AWS_REGION>/$AWS_REGION/g" daemonset.yaml
+sed -i -e "s|<AMP_REMOTE_WRITE_URL>|$AMP_REMOTE_WRITE|g" daemonset.yaml
+kubectl apply -f daemonset.yaml
+echo "sleep 10"
+sleep 10
 kubectl get pods -n prometheus
-kubectl get pods amp-prometheus-chart-server-0  -n prometheus -o jsonpath='{.spec.containers[*].name}*'|  tr -s '[[:space:]]' '\n'
+
 #kubectl port-forward -n prometheus pods/amp-prometheus-chart-server-0 8080:9090
